2867abf81205444b70c1a6428e87589a652e16ff
choigpt <choigptroadbalancer@gmail.com>
2025-07-29 14:08:56 +0900

    refactor : dto 리팩토링
    - NotBlank -> NotNull
    - @Builder 추가
    - 불필요한 메서드 제거(toEntity)

--- src/main/java/com/example/onlyone/domain/notification/dto/NotificationRequestDto.java
+++ src/main/java/com/example/onlyone/domain/notification/dto/NotificationRequestDto.java
@@ -1,28 +1,23 @@
 package com.example.onlyone.domain.notification.dto;
 
-import com.example.onlyone.domain.notification.entity.Notification;
-import com.example.onlyone.domain.notification.entity.NotificationType;
-import com.example.onlyone.domain.user.entity.User;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class NotificationRequestDto {
 
     @NotNull
     @JsonProperty("user_id")
     private Long userId;
 
-    @NotBlank
+    @NotNull
     @JsonProperty("type_code")
     private String typeCode;
 
-    @NotBlank
-    private String content;
+    @NotNull
+    private String[] args;
 
-    public Notification toEntity(User user, NotificationType notificationType) {
-        return Notification.create(user, notificationType, this.content);
-    }
 }
