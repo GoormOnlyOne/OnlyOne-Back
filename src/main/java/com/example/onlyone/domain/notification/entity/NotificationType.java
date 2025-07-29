686f880ec6a703e37efa3b982308ea7486629af2
choigpt <choigptroadbalancer@gmail.com>
2025-07-29 09:48:37 +0900

    docs : 주석 추가

--- src/main/java/com/example/onlyone/domain/notification/entity/NotificationType.java
+++ src/main/java/com/example/onlyone/domain/notification/entity/NotificationType.java
@@ -34,6 +34,7 @@ public class NotificationType extends BaseTimeEntity {
         this.template = template;
     }
 
+    // 알림 유형별로 템플릿을 통해서 렌더링하는 기능
     public String render(String... args) {
         return String.format(this.template, (Object[]) args);
     }
--- src/main/java/com/example/onlyone/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/example/onlyone/domain/notification/repository/NotificationRepository.java
@@ -7,6 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    // 사용자 별로 알림을 최신순으로 정렬해서 조회
     List<Notification> findByUserOrderByCreatedAtDesc(User user);
+
+    // 사용자 별로 읽지 않은 알림들만 조회
     List<Notification> findByUserAndIsReadFalse(User user);
 }
