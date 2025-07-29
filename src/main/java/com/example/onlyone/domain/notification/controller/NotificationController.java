3fe213d856eab0a0579de498c5197964e1504992
choigpt <choigptroadbalancer@gmail.com>
2025-07-29 14:20:10 +0900

    refactor : dto와 엔티티 변환 서비스 레이어에 위임

--- src/main/java/com/example/onlyone/domain/notification/controller/NotificationController.java
+++ src/main/java/com/example/onlyone/domain/notification/controller/NotificationController.java
@@ -2,22 +2,12 @@ package com.example.onlyone.domain.notification.controller;
 
 import com.example.onlyone.domain.notification.dto.NotificationRequestDto;
 import com.example.onlyone.domain.notification.dto.NotificationResponseDto;
-import com.example.onlyone.domain.notification.entity.Notification;
-import com.example.onlyone.domain.notification.entity.Type;
 import com.example.onlyone.domain.notification.service.NotificationService;
-import com.example.onlyone.domain.user.entity.User;
-import com.example.onlyone.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/notifications")
@@ -25,30 +15,23 @@ import java.util.Map;
 public class NotificationController {
 
     private final NotificationService notificationService;
-    private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<Map<String, Object>> sendNotification(@Valid @RequestBody NotificationRequestDto reqDto) {
+    public ResponseEntity<?> sendNotification(
+            @Valid @RequestBody NotificationRequestDto reqDto
+    ) {
 
-        // user_id로 유저 조회
-        User toUser = userService.getMemberById(reqDto.getUserId());
+        NotificationResponseDto responseDto =
+                notificationService.sendNotification(reqDto);
 
-        // enum 변환
-        Type type = Type.valueOf(reqDto.getTypeCode());
-
-        // 알림 생성
-        Notification saved = notificationService.sendNotification(toUser, type, reqDto.getContent());
-
-        // Entity -> Dto
-        NotificationResponseDto data = NotificationResponseDto.fromEntity(saved);
-
-        // 응답 포맷
-        Map<String, Object> result = new LinkedHashMap<>();
-        result.put("success", true);
-        result.put("data", data);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(responseDto);
     }
 
-
+    @PatchMapping("/{id}/read")
+    public ResponseEntity<Void> markAsRead(@PathVariable Long id) {
+        notificationService.markAsRead(id);
+        return ResponseEntity.noContent().build();
+    }
 }
