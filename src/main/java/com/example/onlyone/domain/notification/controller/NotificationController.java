3ba1b0dc8f52e177ed1d4a0df68b57bb98cccca0
choigpt <choigptroadbalancer@gmail.com>
2025-07-30 10:17:24 +0900

    feat : 단건 조회 api 삭제
    - 전부 읽음으로 처리되는 api로 변경

--- src/main/java/com/example/onlyone/domain/notification/controller/NotificationController.java
+++ src/main/java/com/example/onlyone/domain/notification/controller/NotificationController.java
@@ -1,8 +1,10 @@
 package com.example.onlyone.domain.notification.controller;
 
+import com.example.onlyone.domain.notification.dto.NotificationListRequestDto;
 import com.example.onlyone.domain.notification.dto.NotificationRequestDto;
 import com.example.onlyone.domain.notification.dto.NotificationResponseDto;
 import com.example.onlyone.domain.notification.service.NotificationService;
+import com.example.onlyone.global.common.CommonResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -26,12 +28,13 @@ public class NotificationController {
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(responseDto);
+                .body(CommonResponse.success(responseDto));
     }
 
-    @PatchMapping("/{id}/read")
-    public ResponseEntity<Void> markAsRead(@PathVariable Long id) {
-        notificationService.markAsRead(id);
+
+    @PatchMapping("/read-all")
+    public ResponseEntity<Void> readAll(@RequestBody NotificationListRequestDto dto) {
+        notificationService.markAllAsRead(dto);
         return ResponseEntity.noContent().build();
     }
 }
