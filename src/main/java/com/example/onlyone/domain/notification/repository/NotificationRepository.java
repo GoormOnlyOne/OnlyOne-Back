4d5a75fe6ee64703a29745dc495880de269875cc
choigpt <choigptroadbalancer@gmail.com>
2025-07-30 09:54:52 +0900

    feat : 알림 조회시 전체 읽음 처리를 위한 레포지토리 메서드 추가
    - 기존에 사용자 별로 읽지 않은 알림들만 조회하는 기능 삭제
    - 엔티티 순회하는 방식보다 벌크 연산 선택

--- src/main/java/com/example/onlyone/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/example/onlyone/domain/notification/repository/NotificationRepository.java
@@ -3,6 +3,8 @@ package com.example.onlyone.domain.notification.repository;
 import com.example.onlyone.domain.notification.entity.Notification;
 import com.example.onlyone.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -13,4 +15,10 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
 
     // 사용자 별로 읽지 않은 알림들만 조회
     List<Notification> findByUserAndIsReadFalse(User user);
+
+    //알림 조회시 전체 읽음 처리
+    @Modifying(clearAutomatically = true)
+    @Query("update Notification n set n.isRead = true where n.user.userId = :userId and n.isRead = false")
+    int markAllAsReadByUserId(Long userId);
+
 }
