4cafcb101089565d6c29cb792fcdeb58b0952795
choigpt <choigptroadbalancer@gmail.com>
2025-07-30 12:50:09 +0900

    docs : api Tag 추가

--- src/main/java/com/example/onlyone/domain/notification/controller/NotificationController.java
+++ src/main/java/com/example/onlyone/domain/notification/controller/NotificationController.java
@@ -5,12 +5,15 @@ import com.example.onlyone.domain.notification.dto.NotificationRequestDto;
 import com.example.onlyone.domain.notification.dto.NotificationResponseDto;
 import com.example.onlyone.domain.notification.service.NotificationService;
 import com.example.onlyone.global.common.CommonResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "알림")
 @RestController
 @RequestMapping("/notifications")
 @RequiredArgsConstructor
@@ -18,6 +21,8 @@ public class NotificationController {
 
     private final NotificationService notificationService;
 
+
+    @Operation(summary = "알림 생성", description = "알림을 생성합니다")
     @PostMapping
     public ResponseEntity<?> sendNotification(
             @Valid @RequestBody NotificationRequestDto reqDto
@@ -31,7 +36,7 @@ public class NotificationController {
                 .body(CommonResponse.success(responseDto));
     }
 
-
+    @Operation(summary = "전체 알림 읽음", description = "알림 조회시 전체 알림이 읽음 상태로 변합니다")
     @PatchMapping("/read-all")
     public ResponseEntity<Void> readAll(@RequestBody NotificationListRequestDto dto) {
         notificationService.markAllAsRead(dto);
