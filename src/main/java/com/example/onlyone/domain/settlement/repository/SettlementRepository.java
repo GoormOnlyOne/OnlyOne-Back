f91ffccc0c1fde237725a56f04de9bc63b187f52
Hayoung Moon <124586544+gkdudans@users.noreply.github.com>
2025-07-31 15:46:58 +0900

    refactor: 정산/거래 내역 목록 조회 API 로직 수정

--- src/main/java/com/example/onlyone/domain/settlement/repository/SettlementRepository.java
+++ src/main/java/com/example/onlyone/domain/settlement/repository/SettlementRepository.java
@@ -3,10 +3,7 @@ package com.example.onlyone.domain.settlement.repository;
 import com.example.onlyone.domain.schedule.entity.Schedule;
 import com.example.onlyone.domain.settlement.entity.Settlement;
 import com.example.onlyone.domain.settlement.entity.TotalStatus;
-import com.example.onlyone.domain.settlement.entity.UserSettlement;
-import com.example.onlyone.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
 
