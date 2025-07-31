75e74d3d3b763363e37ca31804fa07f48ac8fcf0
choigpt <choigptroadbalancer@gmail.com>
2025-07-31 14:58:00 +0900

    docs : java google convention 적용

--- src/main/java/com/example/onlyone/domain/notification/repository/NotificationTypeRepository.java
+++ src/main/java/com/example/onlyone/domain/notification/repository/NotificationTypeRepository.java
@@ -7,7 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface NotificationTypeRepository extends JpaRepository<NotificationType, Long> {
-
-    // 알림 타입으로 조회(댓글이니?, 정산이니? 등등)
-    Optional<NotificationType> findByType(Type type);
+  Optional<NotificationType> findByType(Type type);
 }
