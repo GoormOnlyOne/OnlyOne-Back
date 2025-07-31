9da74e1fc05c3207fccb4febba3747149bdc6ede
Hayoung Moon <124586544+gkdudans@users.noreply.github.com>
2025-07-31 14:51:54 +0900

    feat: 정산/거래 내역 목록 조회 API 구현

--- src/main/java/com/example/onlyone/domain/wallet/repository/WalletTransactionRepository.java
+++ src/main/java/com/example/onlyone/domain/wallet/repository/WalletTransactionRepository.java
@@ -1,7 +1,17 @@
 package com.example.onlyone.domain.wallet.repository;
 
+import com.example.onlyone.domain.wallet.dto.response.UserWalletTransactionDto;
+import com.example.onlyone.domain.wallet.entity.Type;
+import com.example.onlyone.domain.wallet.entity.Wallet;
 import com.example.onlyone.domain.wallet.entity.WalletTransaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WalletTransactionRepository extends JpaRepository<WalletTransaction,Long> {
+    List<WalletTransaction> findByWalletAndType(Wallet wallet, Type type, Pageable pageable);
+    List<WalletTransaction> findByWalletAndTypeNot(Wallet wallet, Type type, Pageable pageable);
+    List<WalletTransaction> findByWallet(Wallet wallet, Pageable pageable);
 }
