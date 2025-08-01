d5bc3580ba66239e6374fba8aea26962fb0cdc7a
Hayoung Moon <124586544+gkdudans@users.noreply.github.com>
2025-08-01 09:34:57 +0900

    chore: 백엔드 서버 1차 배포

--- src/main/java/com/example/onlyone/OnlyoneApplication.java
+++ src/main/java/com/example/onlyone/OnlyoneApplication.java
@@ -1,11 +1,20 @@
 package com.example.onlyone;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://api.buddkit.p-e.kr", description = "Production Server"),
+                @Server(url = "http://localhost:8080", description = "Local Development Server")
+        }
+)
+
 public class OnlyoneApplication {
 
     public static void main(String[] args) {
