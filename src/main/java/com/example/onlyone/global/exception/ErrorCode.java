7252ea4ae6f874cb3ed3781e74ea964694ce6bee
황인찬 <hwang-inchan@hwang-inchan-ui-MacBookPro.local>
2025-08-01 15:37:49 +0900

    feat: 이미지 처리 관련 에러 코드 추가

--- src/main/java/com/example/onlyone/global/exception/ErrorCode.java
+++ src/main/java/com/example/onlyone/global/exception/ErrorCode.java
@@ -87,6 +87,8 @@ public enum ErrorCode {
     // Image
     INVALID_IMAGE_FOLDER_TYPE(400, "IMAGE_400_1", "유효하지 않은 이미지 폴더 타입입니다."),
     INVALID_IMAGE_CONTENT_TYPE(400, "IMAGE_400_2", "유효하지 않은 이미지 컨텐츠 타입입니다."),
+    INVALID_IMAGE_SIZE(400, "IMAGE_400_3", "유효하지 않은 이미지 크기입니다."),
+    IMAGE_SIZE_EXCEEDED(413, "IMAGE_413_1", "이미지 크기가 허용된 최대 크기(5MB) 크기를 초과했니다."),
     IMAGE_UPLOAD_FAILED(500, "IMAGE_500_1", "이미지 업로드 중 오류가 발생했습니다.");
 
     private final int status;
