ca9b69725c4dd50811506df0bb3803b9e55584bb
NamYeonW00 <114655545+NamYeonW00@users.noreply.github.com>
2025-08-04 17:38:33 +0900

    refact: import 수정
    refact: import 수정

--- src/main/java/com/example/onlyone/domain/chat/repository/MessageRepository.java
+++ src/main/java/com/example/onlyone/domain/chat/repository/MessageRepository.java
@@ -2,13 +2,12 @@ package com.example.onlyone.domain.chat.repository;
 
 import com.example.onlyone.domain.chat.entity.Message;
 
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message,Long> {
