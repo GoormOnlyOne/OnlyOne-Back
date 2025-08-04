d586675e289d53699f5b342b9611f2b37f8552c2
Hayoung Moon <124586544+gkdudans@users.noreply.github.com>
2025-08-04 16:52:38 +0900

    refactor: 사용하지 않는 파일 삭제

--- src/main/java/com/example/onlyone/domain/payment/dto/request/ConfirmTossPayRequest.java
+++ src/main/java/com/example/onlyone/domain/payment/dto/request/ConfirmTossPayRequest.java
@@ -1,6 +1,8 @@
 package com.example.onlyone.domain.payment.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,12 +11,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ConfirmTossPayRequest {
+    @NotBlank
     @JsonProperty("paymentKey")
     private String paymentKey;
 
+    @NotBlank
     @JsonProperty("orderId")
     private String orderId;
 
+    @NotNull
     @JsonProperty("amount")
     private Long amount;
 }
