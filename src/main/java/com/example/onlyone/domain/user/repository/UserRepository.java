ee65a9db8e6ce65ef1e63bb898126ea25554f32c
gelee <geleego@gmail.com>
2025-08-05 15:36:52 +0900

    fix: develop merge 하며 충돌난 부분 해결

--- src/main/java/com/example/onlyone/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/onlyone/domain/user/repository/UserRepository.java
@@ -2,13 +2,11 @@ package com.example.onlyone.domain.user.repository;
 
 import com.example.onlyone.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
+
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
-=======
->>>>>>> develop
 
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByKakaoId(Long kakaoId);
