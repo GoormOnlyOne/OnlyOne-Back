9b6f7617fae6a14428532db6d89eb6144a6c65ce
NamYeonW00 <114655545+NamYeonW00@users.noreply.github.com>
2025-08-06 14:57:31 +0900

    refact: ChatRoom Response 수정
    refact: ChatRoom Response 수정

--- src/main/java/com/example/onlyone/domain/chat/service/ChatRoomService.java
+++ src/main/java/com/example/onlyone/domain/chat/service/ChatRoomService.java
@@ -41,7 +41,8 @@ public class ChatRoomService {
 
     // 유저가 특정 모임(club)의 어떤 채팅방들에 참여하고 있는지 조회
     public List<ChatRoomResponse> getChatRoomsUserJoinedInClub(Long clubId) {
-        Long userId = userService.getCurrentUser().getUserId();
+        //Long userId = userService.getCurrentUser().getUserId();
+        Long userId = 1L;
 
         List<ChatRoom> chatRooms = chatRoomRepository.findChatRoomsByUserIdAndClubId(userId, clubId);
         List<Long> chatRoomIds = chatRooms.stream()
