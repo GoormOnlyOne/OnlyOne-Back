890f3af5f99f684f21fbe8ccbe0f6c8a1dd1a565
Hayoung Moon <124586544+gkdudans@users.noreply.github.com>
2025-08-11 15:26:50 +0900

    refactor: 충전 실패 로직 추가

--- src/main/java/com/example/onlyone/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/example/onlyone/domain/payment/repository/PaymentRepository.java
@@ -1,6 +1,7 @@
 package com.example.onlyone.domain.payment.repository;
 
 import com.example.onlyone.domain.payment.entity.Payment;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +9,6 @@ import java.util.Optional;
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Optional<Payment> findByTossOrderId(String tossOrderId);
     Optional<Payment> findByTossPaymentKey(String tossPaymentKey);
+
+    boolean existsByTossPaymentKey(String paymentKey);
 }
