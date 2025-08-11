4e93f74ff21e76b2a303c7e0fced3a8bd23eea51
Hayoung Moon <124586544+gkdudans@users.noreply.github.com>
2025-08-11 18:42:25 +0900

    refactor: 토스페이먼츠 결제 승인 API 에러 처리 추가

--- src/main/java/com/example/onlyone/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/example/onlyone/domain/payment/repository/PaymentRepository.java
@@ -1,13 +1,15 @@
 package com.example.onlyone.domain.payment.repository;
 
 import com.example.onlyone.domain.payment.entity.Payment;
+import jakarta.persistence.LockModeType;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
-    Optional<Payment> findByTossOrderId(String tossOrderId);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Payment> findByTossPaymentKey(String tossPaymentKey);
 
     boolean existsByTossPaymentKey(String paymentKey);
