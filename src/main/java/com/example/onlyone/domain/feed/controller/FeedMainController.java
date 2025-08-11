5b1a2c79fecc5d20239a05a09b175e9748433aa8
GKYoo <dbehxks02@naver.com>
2025-08-11 19:37:33 +0900

    refact: 스웨거 메서드 요약 이름 변경

--- src/main/java/com/example/onlyone/domain/feed/controller/FeedMainController.java
+++ src/main/java/com/example/onlyone/domain/feed/controller/FeedMainController.java
@@ -29,7 +29,7 @@ import java.util.List;
 public class FeedMainController {
     private final FeedMainService feedMainService;
 
-    @Operation(summary = "전체 피드 목록 조회", description = "유저와 관련된 모든 피드들을 조회합니다.")
+    @Operation(summary = "최신순 피드 목록 조회", description = "유저와 관련된 모든 피드들을 조회합니다.")
     @GetMapping
     public ResponseEntity<?> getAllFeeds(
             @RequestParam(name = "page", defaultValue = "0") int page,
