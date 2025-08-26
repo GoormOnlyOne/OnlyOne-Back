ca9bc9fece471a2c76b32f69f0fce2423799fd6d
Hayoung Moon <124586544+gkdudans@users.noreply.github.com>
2025-08-26 10:23:24 +0900

    test: 결제 관련 테스트 코드 추가
    UT-03-015~UT-03-024

--- src/main/java/com/example/onlyone/domain/wallet/entity/WalletTransaction.java
+++ src/main/java/com/example/onlyone/domain/wallet/entity/WalletTransaction.java
@@ -7,6 +7,8 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "wallet_transaction")
 @Getter
